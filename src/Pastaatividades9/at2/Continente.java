ee22c2f8bba77e5cdc12f59c86df32efb80b03a5
Jeamcarlosbraun <jeamcarlosb@hotmail.com>
2021-07-28 17:25:29 -0300

    atividade3

---
+++ src/Pastaatividades9/at2/Continente.java
@@ -0,0 +1,82 @@
+package Pastaatividades9.at2;
+
+import Pastaatividades9.at1.Pais;
+
+import java.util.ArrayList;
+public class Continente {
+    public String nome;
+    public ArrayList<Pais> paises = new ArrayList<Pais>();
+    public  Continente(String nome){
+        this.nome = nome;
+    }
+    public  void adiciona (Pais p){
+        this.paises.add(p);
+    }
+    public float dimensaoc(){
+        float soma = 0;
+        for(Pais p : this.paises){
+            soma = (float) (soma + p.getDimensao());
+        }
+        return soma;
+    }
+    public int continentepop(){
+        float soma = 0;
+        for(Pais p : this.paises){
+            soma = (soma+p.getPop());
+        }
+        return (int) soma;
+    }
+
+    public float denspop(){
+        return  (float) this.continentepop() / this.dimensaoc();
+    }
+    public Pais maiorPop(){
+        Pais maiorpopulacao = this.paises.get(0);
+
+        for (int i=1; i<this.paises.size();i++){
+            Pais p = this.paises.get(i);
+            if(p.getPop()>maiorpopulacao.getPop()) {
+                maiorpopulacao = p;
+            }
+        }
+        return maiorpopulacao;
+    }
+    public Pais menorPop(){
+        Pais menorpopulacao = this.paises.get(0);
+
+        for (int i=1; i<this.paises.size();i++){
+            Pais p = this.paises.get(i);
+            if(p.getPop() < menorpopulacao.getPop()) {
+                menorpopulacao = p;
+            }
+        }
+        return menorpopulacao;
+    }
+    public Pais maiordimensao(){
+        Pais maiordimensao = this.paises.get(0);
+
+        for (int i=1; i<this.paises.size();i++){
+            Pais p = this.paises.get(i);
+            if(p.getPop()>maiordimensao.getPop()) {
+                maiordimensao = p;
+            }
+        }
+        return maiordimensao;
+    }
+    public Pais menordimensao() {
+        Pais menordimensao = this.paises.get(0);
+
+        for (int i = 1; i < this.paises.size(); i++) {
+            Pais p = this.paises.get(i);
+            if (p.getPop() < menordimensao.getPop()) {
+                menordimensao = p;
+            }
+        }
+        return menordimensao;
+    }
+    public float razao(){
+        Pais maior = this.maiordimensao();
+        Pais menos = this.menordimensao();
+        return (float) (maior.getDimensao()/menos.getDimensao());
+    }
+    }
---
+++ src/Pastaatividades9/at2/Execucao3.java
@@ -0,0 +1,47 @@
+package Pastaatividades9.at2;
+import Pastaatividades9.at1.Pais;
+
+import java.util.ArrayList;
+
+public class Execucao3 {
+    public static void main(String[] args) {
+        Pastaatividades9.at1.Pais p1 = new Pastaatividades9.at1.Pais("BRA", "BRASIL", 2003, 100);
+        Pastaatividades9.at1.Pais p2 = new Pastaatividades9.at1.Pais("ARG", "ARGENTINA", 2000, 100);
+        Pastaatividades9.at1.Pais p3 = new Pastaatividades9.at1.Pais("URU", "URUGUAI", 2040, 100);
+        Pastaatividades9.at1.Pais p4 = new Pastaatividades9.at1.Pais("EQU", "EQUADOR", 2000, 130);
+        Pastaatividades9.at1.Pais p5 = new Pastaatividades9.at1.Pais("BOL", "BOLÍVIA", 2005, 130);
+
+        p1.paisFronteira.add(p2);
+        p1.paisFronteira.add(p3);
+        p1.paisFronteira.add(p5);
+        p2.paisFronteira.add(p1);
+        p2.paisFronteira.add(p3);
+        p2.paisFronteira.add(p5);
+        p3.paisFronteira.add(p1);
+        p3.paisFronteira.add(p2);
+        p5.paisFronteira.add(p1);
+        p5.paisFronteira.add(p2);
+
+        System.out.print("p1 é igual ao p2? ");
+        boolean resp = p1.ehOMesmoPais(p2);
+        System.out.println(resp);
+
+        System.out.print("Argentina faz fronteira com o Brasil?");
+        resp = p2.ehLimitrofe(p1);
+        System.out.println(resp);
+
+        System.out.println("Paises que fazem fronteira com o Brasil e com a Argentina:");
+        ArrayList<Pastaatividades9.at1.Pais> comuns = p1.vizinhosComuns(p2);
+        for(Pais p : comuns){
+            System.out.println(p.getNome());
+        }
+
+
+    Continente america = new Continente("America");
+    america.adiciona(p1);
+    america.adiciona(p2);
+    america.adiciona(p3);
+    america.adiciona(p4);
+    america.adiciona(p5);
+    }
+}
